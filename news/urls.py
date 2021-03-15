776b10ff3bd3d9eb0400270883e2a20f9c298d9b
Jefferson Gakuya <jeffersongakuya@gmail.com>
2021-03-15 21:35:22 +0300

    Bootstrap Installation

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    url('^$', views.welcome, name = 'welcome'),
-    url('^today/$', views.news_of_day, name = 'newsToday'),
-    url('^archives/(\d{4}-\d{2}-\d{2})/$', views.past_days_news, name = 'pastNews')
+    path('index', views.welcome, name = 'welcome'),
+    path('today', views.news_of_day, name = 'newsToday'),
+    path('archives/(\d{4}-\d{2}-\d{2})/$', views.past_days_news, name = 'pastNews')
 ]
