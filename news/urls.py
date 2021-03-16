03739fa5f267bb32c056293d97405d4ec46a0b95
Jefferson Gakuya <jeffersongakuya@gmail.com>
2021-03-16 23:14:38 +0300

    Set the routes

--- news/urls.py
+++ news/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.welcome, name = 'welcome'),
-    path('today/', views.news_of_day, name = 'newsToday'),
+    path('', views.news_of_day, name = 'newsToday'),
     path('archives/<int:year>/<int:month>/<int:day>/', views.past_days_news, name = 'pastNews')
 ]
