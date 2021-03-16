429397245968713caf5bc3cf8dc9d94928d4b6b2
Jefferson Gakuya <jeffersongakuya@gmail.com>
2021-03-16 15:40:27 +0300

    Manage static files

--- news/urls.py
+++ news/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index', views.welcome, name = 'welcome'),
-    path('today', views.news_of_day, name = 'newsToday'),
-    path('archives/(\d{4}-\d{2}-\d{2})/$', views.past_days_news, name = 'pastNews')
+    path('index/', views.welcome, name = 'welcome'),
+    path('today/', views.news_of_day, name = 'newsToday'),
+    path('archives/<int:year>/<int:month>/<int:day>/', views.past_days_news, name = 'pastNews')
 ]
